272612664e0a0bd29b60e7ecb55ae84b3bf81d7b
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-10 09:19:19 +0000

    Finished fizzbuzz

---
+++ fizzbuzz_kata/lib/fizzbuzz.rb
@@ -0,0 +1,19 @@
+def divisible_by_3?(number)
+  number % 3 == 0
+end
+
+def divisible_by_5?(number)
+  number % 5 == 0
+end
+
+def divisible_by_3_and_5?(number)
+  number % 3 == 0 && number % 5 == 0
+end
+
+def fizzbuzz(number)
+  return 0 if number == 0
+  return "fizzbuzz" if divisible_by_3_and_5?(number)
+  return "fizz" if divisible_by_3?(number)
+  return "buzz" if divisible_by_5?(number)
+  number
+end
