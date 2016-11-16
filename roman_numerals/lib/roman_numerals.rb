7a215c19c559efb2d93ebe5ac01fa9f090d1c135
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-16 09:19:04 +0000

    working roman numerals script

---
+++ roman_numerals/lib/roman_numerals.rb
@@ -0,0 +1,79 @@
+def roman_numerals(number)
+  if number >= 1000
+    thousand = thousand_numerals(thousands(number))
+    hundred = hundred_numerals(hundreds(remainder_hundreds(number)))
+    ten = ten_numerals(tens(remainder_tens(number)))
+    one = one_numerals(remainder_ones(number))
+    output = thousand + hundred + ten + one
+  elsif number >= 100
+    hundred = hundred_numerals(hundreds(remainder_hundreds(number)))
+    ten = ten_numerals(tens(remainder_tens(number)))
+    one = one_numerals(remainder_ones(number))
+    output = hundred + ten + one
+  elsif number >= 10
+    ten = ten_numerals(tens(remainder_tens(number)))
+    one = one_numerals(remainder_ones(number))
+    output = ten + one
+  else
+    one_numerals(number)
+  end
+end
+
+def thousands(number)
+  index_thousand = number / 1000
+end
+
+def hundreds(number)
+  index_hundred = number / 100
+end
+
+def tens(number)
+  index_tens = number / 10
+end
+
+def remainder_hundreds(number)
+  remainder_hundreds = number % 1000
+end
+
+def remainder_tens(number)
+  remainder_tens = number % 100
+end
+
+def remainder_ones(number)
+  remainder_ones = number % 10
+end
+
+def tens_array
+  ['X', "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+end
+
+def hundreds_array
+  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+end
+
+def ones_array
+  ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XV"]
+end
+
+def one_numerals(number)
+  return "" if number == 0
+  ones_array[number-1]
+end
+
+def ten_numerals(number)
+  return "" if number == 0
+  tens_array[number-1]
+end
+
+def hundred_numerals(number)
+  return "" if number == 0
+  hundreds_array[number-1]
+end
+
+def thousand_numerals(number)
+  return "" if number == 0
+  "M" * number
+end
+
+
+puts roman_numerals(14)
---
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -0,0 +1,45 @@
+require "roman_numerals"
+
+describe "roman numerals" do
+  it "should contain an array with tens" do
+    expect(tens_array).to eq ['X', "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+  end
+  it "should contain an array with hundreds" do
+    expect(hundreds_array).to eq  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+  end
+  it "should contain an array with the firt numerals" do
+    expect(ones_array).to eq ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "XV"]
+  end
+  context "has an index method that" do
+    it "returns the number of tens" do
+      expect(tens(20)).to eq 2
+    end
+    it "return the number of hundreds" do
+      expect(hundreds(200)).to eq 2
+    end
+    it "return the number of thousands" do
+      expect(thousands(2000)).to eq 2
+    end
+  end
+
+  context "when given a number" do
+    it "1 should return I" do
+      expect(one_numerals(1)).to eq "I"
+    end
+    it "10 should return X" do
+      expect(ten_numerals(tens(10))).to eq "X"
+    end
+    it "100 should return C" do
+      expect(hundred_numerals(hundreds(100))).to eq "C"
+    end
+    it "1000 should return MM" do
+      expect(thousand_numerals(thousands(1000))).to eq "M"
+    end
+  end
+
+  context "When given 0" do
+    it "should return blank" do
+      expect(roman_numerals(0)).to eq ""
+    end
+  end
+end
